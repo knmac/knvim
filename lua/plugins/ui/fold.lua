af388926383934ca000cea4ced555bb53d2ae84c
knmac <khoinguyenx@gmail.com>
2023-04-27 09:45:36 -0700

    ignore fold for some filetypes

--- lua/plugins/ui/fold.lua
+++ lua/plugins/ui/fold.lua
@@ -11,7 +11,6 @@ return {
                     -- foldfunc = 'builtin',
                     -- setopt = true,
                     relculright = true,
-                    ft_ignore = { 'Outline' },
                     segments = {
                         { text = { builtin.foldfunc },      click = 'v:lua.ScFa' },
                         { text = { '%s' },                  click = 'v:lua.ScSa' },
@@ -28,6 +27,17 @@ return {
         vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
+
+        -- Hide fold columnn for some file types
+        local user_cfgs_group = vim.api.nvim_create_augroup('user_cfgs', { clear = false })
+        vim.api.nvim_create_autocmd('FileType', {
+            desc = 'Hide foldcolumn for some file types',
+            pattern = { 'Outline', 'toggleterm', },
+            group = user_cfgs_group,
+            callback = function()
+                vim.o.foldcolumn = '0'
+            end,
+        })
 
         require('ufo').setup({})
     end,
