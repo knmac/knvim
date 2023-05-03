2ea27d76438a3acf8402e9a997a2e1339f2f11d0
knmac <khoinguyenx@gmail.com>
2023-05-02 21:20:40 -0700

    Support wezterm

--- lua/plugins/utils/zen-mode.lua
+++ lua/plugins/utils/zen-mode.lua
@@ -3,8 +3,8 @@ return {
     'folke/zen-mode.nvim',
     event = 'VeryLazy',
     config = function()
-        local map = vim.keymap.set
-        local default_opts = { noremap = true, silent = true }
+        -- local map = vim.keymap.set
+        -- local default_opts = { noremap = true, silent = true }
 
         --- Return with with maximum threshold
         local width_with_max = function(ratio, min_width)
@@ -15,24 +15,28 @@ return {
 
         require('zen-mode').setup({
             window = {
-                backdrop = 0.9,
+                backdrop = 0.95,
                 width = width_with_max(0.85, 150),
             },
             plugins = {
                 kitty = {
                     enabled = false,
-                    font = '+4',
+                    font = '+2',
+                },
+                wezterm = {
+                    enabled = true,
+                    font = '+2',
                 },
             },
             -- callback where you can add custom code when the Zen window opens
             on_open = function()
-                map('n', 'j', 'jzz', default_opts)
-                map('n', 'k', 'kzz', default_opts)
+                -- map('n', 'j', 'jzz', default_opts)
+                -- map('n', 'k', 'kzz', default_opts)
             end,
             -- callback where you can add custom code when the Zen window closes
             on_close = function()
-                map('n', 'j', 'j', default_opts)
-                map('n', 'k', 'k', default_opts)
+                -- map('n', 'j', 'j', default_opts)
+                -- map('n', 'k', 'k', default_opts)
             end,
         })
     end,
