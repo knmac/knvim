63b50476eb217d2dc2a5c9d0b329384e6eb5a470
knmac <khoinguyenx@gmail.com>
2023-06-19 16:02:33 -0700

    Restructure lualine

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,57 +1,60 @@
 -- Statusline at the bottom
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = 'nvim-tree/nvim-web-devicons',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        'nvim-telescope/telescope.nvim',  -- Switch filetype
+        'SmiteshP/nvim-navic',  -- Show navic status
+        'AckslD/swenv.nvim',  -- Show and switch python env
+    },
     config = function()
-        -- Custom winbar config that uses nvim-navic
-        local navic_ok, navic = pcall(require, 'nvim-navic')
-        local winbar_cfg = {}
-        local inactive_winbar_cfg = {}
+        -- Custom components --------------------------------------------------
+        -- Navic status for winbar
+        local navic_stat = {
+            function()
+                local loc = require('nvim-navic').get_location()
+                if loc ~= '' then
+                    return '› ' .. loc
+                end
+                return loc
+            end,
+            cond = function()
+                return require('nvim-navic').is_available()
+            end,
+            color = { bg = 'NONE' },
+        }
 
-        if navic_ok then
-            winbar_cfg = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {
-                    { 'filename', path = 1, color = { bg = 'NONE' } },
-                    {
-                        function()
-                            local loc = navic.get_location()
-                            if loc ~= '' then
-                                return '› ' .. loc
-                            end
-                            return loc
-                        end,
-                        cond = function()
-                            return navic.is_available()
-                        end,
-                        color = { bg = 'NONE' },
-                    },
-                },
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {}
-            }
-            inactive_winbar_cfg = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {
-                    { 'filename', path = 1, color = { bg = 'NONE' } },
-                },
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {}
-            }
-        end
+        -- Message to show the number of spaces per tab of the buffer
+        local fmt_stat = {
+            function()
+                local stat = ''
+                stat = stat .. 'spaces=' .. vim.opt_local.tabstop._value
+                return stat
+            end,
+        }
 
-        -- Custom message to show the number of spaces per tab of the buffer
-        local fmt_stat = function()
-            local stat = ''
-            stat = stat .. 'spaces=' .. vim.opt_local.tabstop._value
-            return stat
-        end
+        -- Python virtual env
+        local env_stat = {
+            function()
+                return require('swenv.api').get_current_venv().name
+            end,
+            icon = ' ',
+            on_click = function()
+                require('swenv.api').pick_venv()
+                vim.cmd.LspRestart()
+            end,
+        }
 
-        -- Main config
+        -- Clickable filetype
+        local filetype_stat = {
+            'filetype',  -- builtin filetype component
+            on_click = function()
+                require('telescope.builtin').filetypes()
+                vim.cmd.LspRestart()
+            end,
+        }
+
+        -- Main config --------------------------------------------------------
         require('lualine').setup({
             options = {
                 icons_enabled = true,
@@ -77,13 +80,22 @@ return {
             },
             sections = {
                 lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = { { 'filename', path = 3, }, 'searchcount' },
-                lualine_x = { fmt_stat, 'encoding', 'fileformat', 'filetype' },
-                lualine_y = { 'location', 'progress' },
-                lualine_z = {
-                    { 'swenv', icon = ' ', color = { fg = '#24273A', }, }
-                },
+                lualine_b = { 'branch',
+                              'diff',
+                              'diagnostics',
+                            },
+                lualine_c = { { 'filename', path = 3, },
+                              'searchcount',
+                            },
+                lualine_x = { fmt_stat,
+                              'encoding',
+                              'fileformat',
+                              filetype_stat,
+                            },
+                lualine_y = { 'location',
+                              'progress',
+                            },
+                lualine_z = { env_stat },
             },
             inactive_sections = {
                 lualine_a = {},
@@ -93,9 +105,26 @@ return {
                 lualine_y = {},
                 lualine_z = {}
             },
+            winbar = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { { 'filename', path = 1, color = { bg = 'NONE' } },
+                              navic_stat,
+                            },
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {}
+            },
+            inactive_winbar = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { { 'filename', path = 1, color = { bg = 'NONE' } },
+                            },
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {}
+            },
             tabline = {},
-            winbar = winbar_cfg,
-            inactive_winbar = inactive_winbar_cfg,
             -- extensions = { 'nvim-tree', 'neo-tree', 'quickfix', 'fugitive', 'symbols-outline', 'toggleterm', 'nvim-dap-ui' }
         })
     end,
