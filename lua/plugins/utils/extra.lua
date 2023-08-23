ab8eaa219ac049772bef469737b4b8251f443b33
knmac <khoinguyenx@gmail.com>
2023-08-23 13:17:12 -0700

    Add diffview

--- lua/plugins/utils/extra.lua
+++ lua/plugins/utils/extra.lua
@@ -3,6 +3,7 @@ return {
     'rlane/pounce.nvim',
     'andymass/vim-matchup',        -- highlight and navigate sets of matching text
     'norcalli/nvim-colorizer.lua', -- colorize color code
+    'sindrets/diffview.nvim',      -- diffview and merge tool
     -- Non-lua nvim plugins
     'junegunn/vim-easy-align',     -- alignment plugin
     'fladson/vim-kitty',           -- syntax highlighting for kitty
