34e6c8b5539942cc4a4fbfb87267f8e2bfd4304b
knmac <khoinguyenx@gmail.com>
2023-09-18 09:05:37 -0700

    Remove Toggle Zoom

--- lua/core/auto_commands.lua
+++ lua/core/auto_commands.lua
@@ -83,23 +83,22 @@ function ClearReg()
 end
 vim.api.nvim_create_user_command('ClearReg', function() ClearReg() end, {})
 
--- Toogle zoom the current window
--- TODO: change to lua implementation
-vim.cmd [[
-function! ToggleZoom(zoom)
-    if exists("t:restore_zoom") && (a:zoom == v:true || t:restore_zoom.win != winnr())
-        exec t:restore_zoom.cmd
-        unlet t:restore_zoom
-    elseif a:zoom
-        let t:restore_zoom = { 'win': winnr(), 'cmd': winrestcmd() }
-        exec "normal \<C-W>\|\<C-W>_"
-    endif
-endfunction
+-- Toggle zoom the current window
+-- vim.cmd [[
+-- function! ToggleZoom(zoom)
+--     if exists("t:restore_zoom") && (a:zoom == v:true || t:restore_zoom.win != winnr())
+--         exec t:restore_zoom.cmd
+--         unlet t:restore_zoom
+--     elseif a:zoom
+--         let t:restore_zoom = { 'win': winnr(), 'cmd': winrestcmd() }
+--         exec "normal \<C-W>\|\<C-W>_"
+--     endif
+-- endfunction
 
-augroup restorezoom
-    au WinEnter * silent! :call ToggleZoom(v:false)
-augroup END
-]]
+-- augroup restorezoom
+--     au WinEnter * silent! :call ToggleZoom(v:false)
+-- augroup END
+-- ]]
 
 -- Fill the rest of line with characters
 -- TODO: change to lua implementation
