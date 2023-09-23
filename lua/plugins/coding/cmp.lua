6e039988a0218b89c61b83084637fcb10df1082e
knmac <khoinguyenx@gmail.com>
2023-09-23 16:11:18 -0700

    Update neorg

--- lua/plugins/coding/cmp.lua
+++ lua/plugins/coding/cmp.lua
@@ -118,13 +118,11 @@ return {
         -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
         cmp.setup.cmdline(':', {
             mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = 'path' }
-            }, {
-                { name = 'cmdline' },
-            }, {
-                { name = 'neorg' },
-            })
+            sources = cmp.config.sources(
+                { { name = 'path' } },
+                { { name = 'cmdline' } },
+                { { name = 'neorg' } }
+            )
         })
 
         -- Load snippets for luasnip
--- lua/plugins/utils/neorg.lua
+++ lua/plugins/utils/neorg.lua
@@ -3,21 +3,24 @@ return {
     'nvim-neorg/neorg',
     dependencies = 'nvim-lua/plenary.nvim',
     build = ':Neorg sync-parsers', -- This is the important bit!
+    event = 'VeryLazy',
     opts = {
         load = {
             ['core.defaults'] = {},
-            ['core.concealer'] = {},
+            ['core.integrations.nvim-cmp'] = {},
             ['core.completion'] = {
                 config = {
                     engine = 'nvim-cmp',
                 },
             },
+            ['core.concealer'] = {},
             ['core.dirman'] = {
                 config = {
                     workspaces = {
                         work = '~/Documents/notes/work',
                         home = '~/Documents/notes/home',
-                    }
+                    },
+                    default_workspace = 'work',
                 }
             },
             ['core.export'] = {},
