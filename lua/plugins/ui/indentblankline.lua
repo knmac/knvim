116790efbc793b298870b6985dfff247291c612c
knmac <khoinguyenx@gmail.com>
2023-09-28 08:49:21 -0700

    Highlight function scope using indent-blankline

--- lua/plugins/ui/indentblankline.lua
+++ lua/plugins/ui/indentblankline.lua
@@ -2,18 +2,48 @@
 return {
     'lukas-reineke/indent-blankline.nvim', -- show indent line
     main = 'ibl',
-    opts = {
-        --char = '|',
-        buftype_exclude = { 'terminal' },
-        filetype_exclude = {
-            'help', 'startify', 'make', 'NvimTree', 'neo-tree', 'neo-tree-popup',
-            'dashboard', 'lsp-installer', 'alpha', 'packer', 'Outline', 'mason',
-            'dap-float', 'dap-repl', 'dapui_scopes', 'dapui_breakpoints',
-            'dapui_stacks', 'dapui_watches', 'dapui_console', 'norg',
-            'lazy', 'lspinfo', 'null-ls-info', 'noice',
-        },
-        use_treesitter = true,
-        show_current_context = true,
-        show_current_context_start = false,
-    },
+    config = function()
+        require('ibl').setup({
+            --char = '|',
+            buftype_exclude = { 'terminal' },
+            filetype_exclude = {
+                'help', 'startify', 'make', 'NvimTree', 'neo-tree', 'neo-tree-popup',
+                'dashboard', 'lsp-installer', 'alpha', 'packer', 'Outline', 'mason',
+                'dap-float', 'dap-repl', 'dapui_scopes', 'dapui_breakpoints',
+                'dapui_stacks', 'dapui_watches', 'dapui_console', 'norg',
+                'lazy', 'lspinfo', 'null-ls-info', 'noice',
+            },
+            use_treesitter = true,
+            show_current_context = true,
+            show_current_context_start = false,
+        })
+
+        -- rainbow-delimiters.nvim integration
+        local highlight = {
+            'RainbowRed',
+            'RainbowYellow',
+            'RainbowBlue',
+            'RainbowOrange',
+            'RainbowGreen',
+            'RainbowViolet',
+            'RainbowCyan',
+        }
+        local hooks = require('ibl.hooks')
+        -- create the highlight groups in the highlight setup hook, so they are reset
+        -- every time the colorscheme changes
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+            vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+            vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+            vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+            vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
+            vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+            vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+        end)
+
+        vim.g.rainbow_delimiters = { highlight = highlight }
+        require('ibl').setup { scope = { highlight = highlight } }
+
+        hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end,
 }
