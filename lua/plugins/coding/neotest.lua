d2a57e0a0764689a2e3c5158aa24dc0f4551c786
knmac <khoinguyenx@gmail.com>
2023-12-01 16:34:50 -0800

    Add neotest

---
+++ lua/plugins/coding/neotest.lua
@@ -0,0 +1,17 @@
+-- Code tester
+return {
+    'nvim-neotest/neotest',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-neotest/neotest-python',
+    },
+    config = function()
+        require('neotest').setup({
+            adapters = {
+                require('neotest-python')({
+                    dap = { justMyCode = false },
+                }),
+            },
+        })
+    end,
+}
