8603b436a4e0f753c77cdd95d59964819061b506
knmac <khoinguyenx@gmail.com>
2023-12-28 08:39:33 +0700

    Cleanup treesitter config

--- lua/plugins/coding/treesitter.lua
+++ lua/plugins/coding/treesitter.lua
@@ -4,18 +4,23 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
+            ensure_installed = { "python", "c", "cpp", "bash", "latex", "bibtex", "markdown",
+                "markdown_inline", "json", "yaml", "toml", "html", "css", "lua", "cmake",
+                "dockerfile", "regex", "sql", "vim", "vimdoc", "javascript", "typescript", "scala",
+                "dap_repl",
+            },
             highlight = {
                 enable = true,
                 -- disable = {},
                 disable = function(_, bufnr)
+                    -- Disable in large number of line
                     if vim.api.nvim_buf_line_count(bufnr) > 50000 then
-                        -- Disable in large number of line
                         return true
                     end
 
+                    -- Disable in large buffer size
                     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
                     if ok and stats and stats.size > 100 * 1024 then
-                        -- Disable in large buffer size
                         return true
                     end
                 end,
@@ -24,18 +29,9 @@ return {
                 enable = false, -- treesitter's indent is buggy
                 disable = {},
             },
-            ensure_installed = { "python", "c", "cpp", "bash", "latex", "bibtex", "markdown",
-                "markdown_inline", "json", "yaml", "toml", "html", "css", "lua", "cmake",
-                "dockerfile", "regex", "sql", "vim", "vimdoc", "javascript", "typescript", "scala",
-                "dap_repl",
-            },
             matchup = {
                 enable = true,
             },
         })
-
-        -- Overwrite fold method using treesitter expression
-        -- vim.opt.foldmethod = "expr"
-        -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
     end,
 }
