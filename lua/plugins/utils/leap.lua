9cd34e814032a2296c4c8d1db18dfeffd67dc72b
knmac <khoinguyenx@gmail.com>
2024-05-20 19:23:00 -0700

    fix typo

--- lua/plugins/utils/leap.lua
+++ lua/plugins/utils/leap.lua
@@ -1,7 +1,9 @@
 -- Fuzzy text jumping
 return {
-    dependency = { "tpope/vim-repeat" },
     "ggandor/leap.nvim",
+    dependencies = {
+        "tpope/vim-repeat",
+    },
     config = function()
         require("leap").create_default_mappings()
     end,
