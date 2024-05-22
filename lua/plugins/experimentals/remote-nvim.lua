7e364dd97081c7eb88d3f698ed1dd8092cbd826d
knmac <khoinguyenx@gmail.com>
2024-05-22 09:38:56 -0700

    update experimental remote-plugin

--- lua/plugins/experimentals/remote-nvim.lua
+++ lua/plugins/experimentals/remote-nvim.lua
@@ -1,12 +1,48 @@
 return {
     "amitds1997/remote-nvim.nvim",
-    version = "*", -- This keeps it pinned to semantic releases
+    version = "*",                       -- Pin to Github releases
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify",
-        -- This would be an optional dependency eventually
-        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim",         -- For standard functions
+        "MunifTanjim/nui.nvim",          -- To build the plugin UI
+        "nvim-telescope/telescope.nvim", -- For picking b/w different remoted methods
     },
-    config = true, -- This calls the default setup(); make sure to call it
+    config = function()
+        require("remote-nvim").setup({
+            remote = {
+                copy_dirs = {
+                    -- What to copy to remote's Neovim config directory
+                    config = {
+                        -- TODO: this is buggy because knvim config is ~/.config/knvim. But this
+                        -- will copy to ~/.remote-nvim/workspaces/[workspace_name]/.config/knvim and
+                        -- use the config from
+                        -- ~/.remote-nvim/workspaces/[workspace_name]/.confg/nvim, so we need to
+                        -- manually link the 2 folders on remote side. Futhermore, it will copy
+                        -- everything from .git/ folder, which is not necessary.
+                        -- Solution: (1) specify source and target dir, and (2) allow ignore
+                        -- files/dirs
+                        base = vim.fn.stdpath("config"),     -- Path from where data has to be copied
+                        dirs = { "*" }, -- Directories that should be copied over. "*" means all directories. To specify a subset, use a list like {"lazy", "mason"} where "lazy", "mason" are subdirectories
+                    },
+                },
+            },
+            client_callback = function(port, workspace_config)
+                local cmd = ("wezterm cli set-tab-title --pane-id $(wezterm cli spawn nvim --server localhost:%s --remote-ui) %s")
+                    :format(
+                        port,
+                        ("'Remote: %s'"):format(workspace_config.host)
+                    )
+                if vim.env.TERM == "xterm-kitty" then
+                    cmd = ("kitty -e nvim --server localhost:%s --remote-ui"):format(port)
+                end
+                vim.fn.jobstart(cmd, {
+                    detach = true,
+                    on_exit = function(job_id, exit_code, event_type)
+                        -- This function will be called when the job exits
+                        print("Client", job_id, "exited with code", exit_code, "Event type:",
+                            event_type)
+                    end,
+                })
+            end,
+        })
+    end,
 }
