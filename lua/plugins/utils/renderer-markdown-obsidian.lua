874f4eaf78859408d0ad11b2612602e74ded0331
knmac <khoinguyenx@gmail.com>
2024-09-10 13:52:20 -0700

    dynamic workspace for obsidian

--- lua/plugins/utils/renderer-markdown-obsidian.lua
+++ lua/plugins/utils/renderer-markdown-obsidian.lua
@@ -1,36 +1,4 @@
 -- Writing and navigating an Obsidian vault
-
-local toggle_checkbox = function(opts, line_num)
-    -- Allow line_num to be optional, defaulting to the current line if not provided
-    line_num = line_num or unpack(vim.api.nvim_win_get_cursor(0))
-    local line = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, false)[1]
-
-    local checkbox_pattern = "^%s*- %[.] "
-    local checkboxes = opts or { " ", "x" }
-
-    if not string.match(line, checkbox_pattern) then
-        local unordered_list_pattern = "^(%s*)[-*+] (.*)"
-        if string.match(line, unordered_list_pattern) then
-            line = string.gsub(line, unordered_list_pattern, "%1- [ ] %2")
-        else
-            line = string.gsub(line, "^(%s*)", "%1- [ ] ")
-        end
-    else
-        for i, check_char in require("obsidian.itertools").enumerate(checkboxes) do
-            if string.match(line, "^%s*- %[" .. require("obsidian").util.escape_magic_characters(check_char) .. "%].*") then
-                if i == #checkboxes then
-                    i = 0
-                end
-                line = require("obsidian").util.string_replace(line, "- [" .. check_char .. "]",
-                    "- [" .. checkboxes[i + 1] .. "]", 1)
-                break
-            end
-        end
-    end
-    -- 0-indexed
-    vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, { line })
-end
-
 return {
     -- Render markdown
     {
@@ -50,15 +18,6 @@ return {
                     end
                 end,
                 desc = "RenderMarkdown: Toggle rendering"
-            },
-            {
-                "<C-space>",
-                function()
-                    if vim.bo.filetype == "markdown" then
-                        return toggle_checkbox({ " ", "x", "~" })
-                    end
-                end,
-                desc = "RenderMarkdown: Cycle through checkboxes"
             },
         },
         config = function()
@@ -104,8 +63,24 @@ return {
                     name = "personal",
                     path = "~/Documents/vaults/personal",
                 },
-                -- { name = "global", path = "~" },
+                {
+                    name = "no-vault",
+                    path = function()
+                        -- alternatively use the CWD:
+                        -- return assert(vim.fn.getcwd())
+                        return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+                    end,
+                    overrides = {
+                        notes_subdir = vim.NIL,  -- have to use 'vim.NIL' instead of 'nil'
+                        new_notes_location = "current_dir",
+                        templates = {
+                            folder = vim.NIL,
+                        },
+                        disable_frontmatter = true,
+                    },
+                },
             },
+            -- { name = "global", path = "~" },
             mappings = {
                 -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
                 ["gf"] = {
@@ -115,12 +90,12 @@ return {
                     opts = { noremap = false, expr = true, buffer = true, desc = "Obsidian: Go to file under the cursor" },
                 },
                 -- Toggle check-boxes
-                -- ["<C-space>"] = {
-                --     action = function()
-                --         return require("obsidian").util.toggle_checkbox()
-                --     end,
-                --     opts = { buffer = true, desc = "Obsidian: Toggle checkboxes" },
-                -- },
+                ["<C-space>"] = {
+                    action = function()
+                        return require("obsidian").util.toggle_checkbox({" ", "x", "~"})
+                    end,
+                    opts = { buffer = true, desc = "Obsidian: Toggle checkboxes" },
+                },
             },
             ui = {
                 enable = true,
