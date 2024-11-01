700953392c0d460a2d5ad67a04dfb21bcb6e7e7b
knmac <khoinguyenx@gmail.com>
2024-11-01 15:22:16 -0700

    remove deprecated vim.lsp.handlers

--- lua/plugins/coding/lspconfig-mason.lua
+++ lua/plugins/coding/lspconfig-mason.lua
@@ -218,18 +218,6 @@ return {
             ---------------------------------------------------------------------------------------
             -- Setup UI
             ---------------------------------------------------------------------------------------
-            -- -- Popped up window borders
-            -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-            --     vim.lsp.handlers.hover, {
-            --         border = "rounded",
-            --     }
-            -- )
-            -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-            --     vim.lsp.handlers.signature_help, {
-            --         border = "rounded",
-            --         close_events = { "CursorMoved", "BufHidden", "InsertCharPre" },
-            --     }
-            -- )
             require("lspconfig.ui.windows").default_options.border = "rounded"
 
             -- Diagnostic signs
