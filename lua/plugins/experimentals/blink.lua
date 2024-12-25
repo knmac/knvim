ba45ecd12d5daf5cfe26a479acaa86fee945c0a7
knmac <khoinguyenx@gmail.com>
2024-12-25 08:53:15 +0700

    Update experimental blink config

--- lua/plugins/experimentals/blink.lua
+++ lua/plugins/experimentals/blink.lua
@@ -1,83 +1,119 @@
 return {
     "saghen/blink.cmp",
-    -- optional: provides snippets for the snippet source
-    dependencies = "rafamadriz/friendly-snippets",
-
-    -- use a release tag to download pre-built binaries
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+        "onsails/lspkind.nvim",
+    },
     version = "*",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
     opts = {
-        -- 'default' for mappings similar to built-in completion
-        -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-        -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-        -- See the full "keymap" documentation for information on defining your own keymap.
-        keymap = { preset = "default" },
-
-        appearance = {
-            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- Useful for when your theme doesn't support blink.cmp
-            -- Will be removed in a future release
-            use_nvim_cmp_as_default = true,
-            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = "Nerd Font Mono"
+        keymap = {
+            ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+            ["<C-c>"] = { "hide", "fallback" },
+            ["<CR>"] = { "accept", "fallback" },
+            ["<Tab>"] = {
+                function(cmp) return cmp.select_next() end,
+                "snippet_forward",
+                "fallback",
+            },
+            ["<S-Tab>"] = {
+                function(cmp) return cmp.select_prev() end,
+                "snippet_backward",
+                "fallback",
+            },
+            ["<Up>"] = { "select_prev", "fallback" },
+            ["<Down>"] = { "select_next", "fallback" },
+            ["<C-p>"] = { "select_prev", "fallback" },
+            ["<C-n>"] = { "select_next", "fallback" },
+            ["<C-k>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-j>"] = { "scroll_documentation_down", "fallback" },
+            ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-d>"] = { "scroll_documentation_down", "fallback" },
         },
-
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        appearance = {
+            use_nvim_cmp_as_default = false,
+            nerd_font_variant = "mono"
+        },
         sources = {
             default = { "lsp", "path", "snippets", "buffer" },
+            cmdline = function()
+                local type = vim.fn.getcmdtype()
+                -- Search forward and backward
+                if type == "/" or type == "?" then
+                    return { "buffer" }
+                end
+                -- Commands
+                if type == ":" then
+                    return { "cmdline" }
+                end
+                return {}
+            end,
+            providers = {
+                lsp = {
+                    min_keyword_length = 0, -- Number of characters to trigger porvider
+                    score_offset = 0,       -- Boost/penalize the score of the items
+                },
+                path = {
+                    min_keyword_length = 0,
+                },
+                snippets = {
+                    min_keyword_length = 2,
+                },
+                buffer = {
+                    min_keyword_length = 5,
+                    max_items = 5,
+                },
+            },
         },
-
         completion = {
-            -- 'prefix' will fuzzy match on the text before the cursor
-            -- 'full' will fuzzy match on the text before *and* after the cursor
-            -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
             keyword = { range = "full" },
-
-            -- Disable auto brackets
-            -- NOTE: some LSPs may add auto brackets themselves anyway
             accept = { auto_brackets = { enabled = false }, },
-
-            -- Insert completion item on selection, don't select by default
-            list = { selection = "auto_insert" },
-
+            list = {
+                selection = function(ctx)
+                    return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+                end,
+            },
             menu = {
-                -- Don't automatically show the completion menu
                 auto_show = true,
-
-                -- nvim-cmp style menu
                 draw = {
                     columns = {
-                        { "label",    "label_description", gap = 1 },
-                        { "kind_icon" },
-                        { "kind" }
+                        { "label",     "label_description", gap = 1 },
+                        { "kind_icon", "kind" },
+                    },
+                    components = {
+                        kind_icon = {
+                            text = function(item)
+                                local kind = require("lspkind").symbol_map[item.kind] or ""
+                                return kind .. " "
+                            end,
+                            highlight = "CmpItemKind",
+                        },
+                        label = {
+                            text = function(item)
+                                return item.label
+                            end,
+                            highlight = "CmpItemAbbr",
+                        },
+                        kind = {
+                            text = function(item)
+                                return item.kind
+                            end,
+                            highlight = "CmpItemKind",
+                        },
                     },
                 },
                 border = "rounded",
-                winblend = 0,
             },
-
-            -- Show documentation when selecting a completion item
             documentation = {
                 auto_show = true,
                 auto_show_delay_ms = 100,
-                window = { border = "rounded", winblend = 0, },
+                treesitter_highlighting = true,
+                window = { border = "rounded" },
             },
-
-            -- Display a preview of the selected item on the current line
             ghost_text = { enabled = true },
         },
-
         -- Experimental signature help support
         signature = {
-            enabled = true,
+            enabled = false,
             trigger = {
                 blocked_trigger_characters = {},
                 blocked_retrigger_characters = {},
@@ -91,7 +127,7 @@ return {
                 border = "rounded",
                 winblend = 0,
                 winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
-                scrollbar = false, -- Note that the gutter will be disabled when border ~= 'none'
+                scrollbar = true, -- Note that the gutter will be disabled when border ~= 'none'
                 -- Which directions to show the window,
                 -- falling back to the next direction when there's not enough space,
                 -- or another window is in the way
