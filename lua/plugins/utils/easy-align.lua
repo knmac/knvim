25382175375695b3f9a230c532748770f494c8b6
knmac <khoinguyenx@gmail.com>
2025-01-15 10:14:41 +0700

    Clean up

--- lua/plugins/utils/easy-align.lua
+++ lua/plugins/utils/easy-align.lua
@@ -1,12 +1,11 @@
 -- A simple, easy-to-use Vim alignment plugin
 return {
-    {
-        "junegunn/vim-easy-align",
-        keys = {
-            -- Start interactive EasyAlign in visual mode (e.g. vip\a)
-            { "<leader>a", "<Plug>(EasyAlign)", mode = "x", desc = "EasyAlign: Start interactive EasyAlign in visual mode" },
-            -- Start interactive EasyAlign for a motion/text object (e.g. \aip)
-            { "<leader>a", "<Plug>(EasyAlign)", mode = "n", desc = "EasyAlign: Start interactive EasyAlign for a motion/text object" },
-        },
+    "junegunn/vim-easy-align",
+    event = "VeryLazy",
+    keys = {
+        -- Start interactive EasyAlign in visual mode (e.g. vip\a)
+        { "<leader>a", "<Plug>(EasyAlign)", mode = "x", desc = "EasyAlign: Start interactive EasyAlign in visual mode" },
+        -- Start interactive EasyAlign for a motion/text object (e.g. \aip)
+        { "<leader>a", "<Plug>(EasyAlign)", mode = "n", desc = "EasyAlign: Start interactive EasyAlign for a motion/text object" },
     },
 }
