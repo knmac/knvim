1f251e31b8242fc527012faec907cf62ae8a6c3c
knmac <khoinguyenx@gmail.com>
2025-02-20 09:46:26 -0800

    Update icon appearance for blink
    Use dropbar icons

--- lua/plugins/coding/blink.lua
+++ lua/plugins/coding/blink.lua
@@ -10,6 +10,81 @@ return {
         {
             "onsails/lspkind.nvim",
             event = { "CmdlineEnter", "InsertEnter" },
+            opts = {
+                symbol_map = {
+                    Array = "󰅪 ",
+                    Boolean = " ",
+                    BreakStatement = "󰙧 ",
+                    Call = "󰃷 ",
+                    CaseStatement = "󱃙 ",
+                    Class = " ",
+                    Color = "󰏘 ",
+                    Constant = "󰏿 ",
+                    Constructor = " ",
+                    ContinueStatement = "→ ",
+                    Copilot = " ",
+                    Declaration = "󰙠 ",
+                    Delete = "󰩺 ",
+                    DoStatement = "󰑖 ",
+                    Enum = " ",
+                    EnumMember = " ",
+                    Event = " ",
+                    Field = " ",
+                    File = "󰈔 ",
+                    Folder = "󰉋 ",
+                    ForStatement = "󰑖 ",
+                    Function = "󰊕 ",
+                    H1Marker = "󰉫 ", -- Used by markdown treesitter parser
+                    H2Marker = "󰉬 ",
+                    H3Marker = "󰉭 ",
+                    H4Marker = "󰉮 ",
+                    H5Marker = "󰉯 ",
+                    H6Marker = "󰉰 ",
+                    Identifier = "󰀫 ",
+                    IfStatement = "󰇉 ",
+                    Interface = " ",
+                    Keyword = "󰌋 ",
+                    List = "󰅪 ",
+                    Log = "󰦪 ",
+                    Lsp = " ",
+                    Macro = "󰁌 ",
+                    MarkdownH1 = "󰉫 ", -- Used by builtin markdown source
+                    MarkdownH2 = "󰉬 ",
+                    MarkdownH3 = "󰉭 ",
+                    MarkdownH4 = "󰉮 ",
+                    MarkdownH5 = "󰉯 ",
+                    MarkdownH6 = "󰉰 ",
+                    Method = "󰆧 ",
+                    Module = "󰏗 ",
+                    Namespace = "󰅩 ",
+                    Null = "󰢤 ",
+                    Number = "󰎠 ",
+                    Object = "󰅩 ",
+                    Operator = "󰆕 ",
+                    Package = "󰆦 ",
+                    Pair = "󰅪 ",
+                    Property = " ",
+                    Reference = "󰦾 ",
+                    Regex = " ",
+                    Repeat = "󰑖 ",
+                    Scope = "󰅩 ",
+                    Snippet = "󰩫 ",
+                    Specifier = "󰦪 ",
+                    Statement = "󰅩 ",
+                    String = "󰉾 ",
+                    Struct = " ",
+                    SwitchStatement = "󰺟 ",
+                    Table = "󰅩 ",
+                    Terminal = " ",
+                    Text = " ",
+                    Type = " ",
+                    TypeParameter = "󰆩 ",
+                    Unit = " ",
+                    Value = "󰎠 ",
+                    Variable = "󰀫 ",
+                    WhileStatement = "󰑖 ",
+                },
+            },
         },
     },
     version = "*",
@@ -53,13 +128,38 @@ return {
                 draw = {
                     columns = {
                         { "label",     "label_description", gap = 1 },
-                        { "kind_icon", "kind" },
+                        { "kind_icon", "kind",              gap = 0 },
                     },
                     components = {
                         kind_icon = {
-                            text = function(item)
-                                local kind = require("lspkind").symbol_map[item.kind] or ""
-                                return kind .. " "
+                            ellipsis = false,
+                            text = function(ctx)
+                                local icon = ctx.kind_icon
+                                if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                                    if dev_icon then
+                                        icon = dev_icon
+                                    end
+                                else
+                                    icon = require("lspkind").symbolic(ctx.kind, { mode = "symbol", })
+                                end
+
+                                return icon .. ctx.icon_gap
+                            end,
+
+                            -- Optionally, use the highlight groups from nvim-web-devicons
+                            -- You can also add the same function for `kind.highlight` if you want to
+                            -- keep the highlight groups in sync with the icons.
+                            highlight = function(ctx)
+                                local hl = "BlinkCmpKind" .. ctx.kind
+                                    or require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx)
+                                if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                                    if dev_icon then
+                                        hl = dev_hl
+                                    end
+                                end
+                                return hl
                             end,
                         },
                     },
