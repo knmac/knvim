11a1b4e22ae1d2039a9239f2e9eff61b6d6cd9e4
knmac <khoinguyenx@gmail.com>
2025-03-31 13:44:47 -0700

    Change default catppuccin integration

--- lua/plugins/ui/catppuccin.lua
+++ lua/plugins/ui/catppuccin.lua
@@ -7,11 +7,11 @@ return {
             flavour = "macchiato", -- latte, frappe, macchiato, mocha
             transparent_background = false,
             term_colors = true,
-            dim_inactive = {
-                enabled = false,
-                shade = "dark",
-                percentage = 0.15,
-            },
+            -- dim_inactive = {
+            --     enabled = false,
+            --     shade = "dark",
+            --     percentage = 0.15,
+            -- },
             styles = {
                 comments = { "italic" },
                 conditionals = { "bold" },
@@ -29,39 +29,20 @@ return {
             integrations = {
                 barbar = true,
                 blink_cmp = true,
-                cmp = true,
                 colorful_winsep = {
                     enabled = true,
                     color = "mauve",
                 },
-                dropbar = {
-                    enabled = true,
-                    color_mode = true,
-                },
-                gitsigns = true,
-                leap = true,
-                neotree = true,
-                noice = true,
-                notify = true,
-                nvimtree = true,
-                snacks = true,
-                symbols_outline = true,
-                treesitter = true,
+                -- dropbar = {
+                --     enabled = true,
+                --     color_mode = true,
+                -- },
+                -- noice = true,
+                -- notify = true,
+                -- snacks = true,
+                -- symbols_outline = true,
                 rainbow_delimiters = true,
-                which_key = true,
-                indent_blankline = {
-                    enabled = true,
-                    colored_indent_levels = false,
-                },
-                navic = {
-                    enabled = true,
-                    custom_bg = "NONE",
-                },
-                dap = {
-                    enabled = true,
-                    enable_ui = true, -- enable nvim-dap-ui
-                },
-                window_picker = true,
+                -- which_key = true,
             }
         })
 
