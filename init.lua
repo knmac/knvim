e67b7ace61e50a9dbcf6619815782606ac2aecf9
knmac <khoinguyenx@gmail.com>
2025-04-04 13:13:55 -0700

    Clean up

--- init.lua
+++ init.lua
@@ -18,8 +18,7 @@ local plugins_cfg_dir = "plugins"
 ---@diagnostic disable-next-line: undefined-field
 if not vim.uv.fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo,
-        lazypath })
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -36,9 +35,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         -- import your plugins
-        {
-            import = plugins_cfg_dir,
-        },
+        { import = plugins_cfg_dir, },
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
