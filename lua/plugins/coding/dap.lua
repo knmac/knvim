a77c930d1ffbd41e60380d935ba7adfcecdeda65
knmac <khoinguyenx@gmail.com>
2025-05-12 13:32:18 -0700

    Refactor code for DAP

--- lua/plugins/coding/dap.lua
+++ lua/plugins/coding/dap.lua
@@ -36,185 +36,194 @@ local generate_python_launher = function()
 end
 
 return {
-    "mfussenegger/nvim-dap", -- debug adapter protocol
-    event = { "BufReadPre", "BufNewFile" },
-    -- event = "VeryLazy",
-    dependencies = {
-        -- {
-        --     "rcarriga/nvim-dap-ui", -- UI for nvim-dap
-        --     event = { "BufReadPre", "BufNewFile" },
-        --     -- event = "VeryLazy",
-        --     keys = {
-        --         { ",d", function() require("dapui").toggle() end, desc = "DAP: Toggle UI" },
-        --     },
-        --     opts = {},
-        -- },
-        {
-            "jay-babu/mason-nvim-dap.nvim", -- bridges mason.nvim and nvim-dap
-            event = { "BufReadPre", "BufNewFile" },
-            -- event = "VeryLazy",
-            opts = {
-                ensure_installed = { "python", "codelldb", "bash" },
-                automatic_installation = true,
-            },
-        },
-        {
-            "igorlfs/nvim-dap-view",
-            event = "VeryLazy",
-            keys = {
-                { ",d", function() require("dap-view").toggle() end, desc = "DAP: Toggle UI" },
-            },
-            opts = {},
-        },
-        {
-            "LiadOz/nvim-dap-repl-highlights", -- syntax highlights to nvim-dap REPL
-            event = { "BufReadPre", "BufNewFile" },
-            -- event = "VeryLazy",
-            dependencies = "nvim-treesitter/nvim-treesitter",
-            build = ":TSInstall dap_repl",
-            opts = {},
+    -- ────────────────────────────────────────────────────────────────────────────────────────────
+    -- Mason DAP
+    -- ────────────────────────────────────────────────────────────────────────────────────────────
+    {
+        "jay-babu/mason-nvim-dap.nvim", -- bridges mason.nvim and nvim-dap
+        event = { "BufReadPre", "BufNewFile" },
+        -- event = "VeryLazy",
+        opts = {
+            ensure_installed = { "python", "codelldb", "bash" },
+            automatic_installation = true,
         },
     },
-    keys = {
-        -- { ",D", function() require("dap").repl.toggle() end,       desc = "DAP: Open default REPL" },
-        { ",c", function() require("dap").continue() end,          desc = "DAP: Start/Continue debugging", },
-        { ",l", function() require("dap").run_last() end,          desc = "DAP: Run the last debug adapter entry" },
-        { ",b", function() require("dap").toggle_breakpoint() end, desc = "DAP: Toggle breakpoint" },
-        { ",n", function() require("dap").step_over() end,         desc = "DAP: Step over" },
-        { ",s", function() require("dap").step_into() end,         desc = "DAP: Step into" },
-        { ",u", function() require("dap").step_out() end,          desc = "DAP: Step out" },
-        { ",t", function() require("dap").terminate() end,         desc = "DAP: Terminate debugging" },
-        {
-            ",B",
-            function()
-                vim.ui.input(
-                    { prompt = "Breakpoint condition: " },
-                    function(input)
-                        require("dap").set_breakpoint(input)
-                    end
-                )
-            end,
-            desc = "DAP: Toggle breakpoint with condition",
+    -- ────────────────────────────────────────────────────────────────────────────────────────────
+    -- DAP view
+    -- ────────────────────────────────────────────────────────────────────────────────────────────
+    {
+        "igorlfs/nvim-dap-view",
+        event = { "BufReadPre", "BufNewFile" },
+        -- event = "VeryLazy",
+        keys = {
+            { ",d", function() require("dap-view").toggle() end, desc = "DAP: Toggle UI" },
         },
-        { ",g", generate_python_launher, desc = "DAP: Generate launcher for Python" },
-        {
-            ",k",
-            function() require("dap.ui.widgets").hover(nil, { border = "rounded" }) end,
-            desc = "DAP: Check variable value on hover",
-        },
-        {
-            ",S",
-            function()
-                local widgets = require("dap.ui.widgets")
-                widgets.centered_float(widgets.scopes, { border = "rounded" })
-            end,
-            desc = "DAP: Open scope",
-        },
+        opts = {},
     },
-    config = function()
-        local dap = require("dap")
-
-        -- ────────────────────────────────────────────────────────────────────────────────────────
-        -- Configurations for each languages
-        -- ────────────────────────────────────────────────────────────────────────────────────────
-        -- Python - debugpy
-        dap.adapters.python = {
-            type = "executable",
-            command = vim.g.python3_host_prog,
-            args = { "-m", "debugpy.adapter", },
-        }
-        dap.configurations.python = {
+    {
+        "LiadOz/nvim-dap-repl-highlights",         -- syntax highlights to nvim-dap REPL
+        event = { "BufReadPre", "BufNewFile" },
+        -- event = "VeryLazy",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        build = ":TSInstall dap_repl",
+        opts = {},
+    },
+    -- {
+    --     "rcarriga/nvim-dap-ui", -- UI for nvim-dap
+    --     event = { "BufReadPre", "BufNewFile" },
+    --     -- event = "VeryLazy",
+    --     keys = {
+    --         { ",d", function() require("dapui").toggle() end, desc = "DAP: Toggle UI" },
+    --     },
+    --     opts = {},
+    -- },
+    -- ────────────────────────────────────────────────────────────────────────────────────────────
+    -- DAP
+    -- ────────────────────────────────────────────────────────────────────────────────────────────
+    {
+        "mfussenegger/nvim-dap", -- debug adapter protocol
+        event = { "BufReadPre", "BufNewFile" },
+        -- event = "VeryLazy",
+        dependencies = {
+        },
+        keys = {
+            -- { ",D", function() require("dap").repl.toggle() end,       desc = "DAP: Open default REPL" },
+            { ",c", function() require("dap").continue() end,          desc = "DAP: Start/Continue debugging", },
+            { ",l", function() require("dap").run_last() end,          desc = "DAP: Run the last debug adapter entry" },
+            { ",b", function() require("dap").toggle_breakpoint() end, desc = "DAP: Toggle breakpoint" },
+            { ",n", function() require("dap").step_over() end,         desc = "DAP: Step over" },
+            { ",s", function() require("dap").step_into() end,         desc = "DAP: Step into" },
+            { ",u", function() require("dap").step_out() end,          desc = "DAP: Step out" },
+            { ",t", function() require("dap").terminate() end,         desc = "DAP: Terminate debugging" },
             {
-                type = "python",
-                request = "launch",
-                name = "[Default] Launch DAP (debugpy) for the current file",
-                program = "${file}",
-                console = "integratedTerminal",
-                cwd = "${workspaceFolder}",
-                repl_lang = "javascript",
-                args = {},
-            },
-            {
-                type = "python",
-                request = "launch",
-                name = "[Default] Launch DAP (debugpy) with file selection",
-                program = function()
-                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                ",B",
+                function()
+                    vim.ui.input(
+                        { prompt = "Breakpoint condition: " },
+                        function(input)
+                            require("dap").set_breakpoint(input)
+                        end
+                    )
                 end,
-                console = "integratedTerminal",
-                cwd = "${workspaceFolder}",
-                repl_lang = "javascript",
-                args = {},
+                desc = "DAP: Toggle breakpoint with condition",
             },
-        }
-
-        -- C/C++ - codelldb
-        -- NOTE:your code has to be compiled first, e.g., using
-        -- g++ -g main.cpp -o [output_name]
-        -- Then provide [output_name] as the program name
-        dap.adapters.lldb = {
-            type = "executable",
-            command = vim.fn.stdpath("data") .. "/mason/packages/codelldb/codelldb",
-            name = "lldb",
-        }
-        dap.configurations.cpp = {
+            { ",g", generate_python_launher, desc = "DAP: Generate launcher for Python" },
             {
-                type = "lldb",
-                request = "launch",
-                name = "[Default] Launch DAP (codelldb) with file selection",
-                program = function()
-                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                ",k",
+                function() require("dap.ui.widgets").hover(nil, { border = "rounded" }) end,
+                desc = "DAP: Check variable value on hover",
+            },
+            {
+                ",S",
+                function()
+                    local widgets = require("dap.ui.widgets")
+                    widgets.centered_float(widgets.scopes, { border = "rounded" })
                 end,
-                cwd = "${workspaceFolder}",
-                stopOnEntry = false,
-                args = {},
+                desc = "DAP: Open scope",
             },
-        }
-        dap.configurations.c = dap.configurations.cpp
-        dap.configurations.rust = dap.configurations.cpp
+        },
+        init = function()
+            local dap = require("dap")
 
-        -- Bash - bashdb
-        dap.adapters.bashdb = {
-            type = "executable",
-            command = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
-            name = "bashdb",
-        }
-        dap.configurations.sh = {
-            {
-                type = "bashdb",
-                request = "launch",
-                name = "Launch file",
-                showDebugOutput = true,
-                pathBashdb = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
-                pathBashdbLib = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
-                trace = true,
-                file = "${file}",
-                program = "${file}",
-                cwd = "${workspaceFolder}",
-                pathCat = "cat",
-                pathBash = "bash",
-                pathMkfifo = "mkfifo",
-                pathPkill = "pkill",
-                args = {},
-                env = {},
-                terminalKind = "integrated",
+            -- Configurations for each languages ──────────────────────────────────────────────────
+            -- Python - debugpY
+            dap.adapters.python = {
+                type = "executable",
+                command = vim.g.python3_host_prog,
+                args = { "-m", "debugpy.adapter", },
             }
-        }
+            dap.configurations.python = {
+                {
+                    type = "python",
+                    request = "launch",
+                    name = "[Default] Launch DAP (debugpy) for the current file",
+                    program = "${file}",
+                    console = "integratedTerminal",
+                    cwd = "${workspaceFolder}",
+                    repl_lang = "javascript",
+                    args = {},
+                },
+                {
+                    type = "python",
+                    request = "launch",
+                    name = "[Default] Launch DAP (debugpy) with file selection",
+                    program = function()
+                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                    end,
+                    console = "integratedTerminal",
+                    cwd = "${workspaceFolder}",
+                    repl_lang = "javascript",
+                    args = {},
+                },
+            }
 
-        -- ────────────────────────────────────────────────────────────────────────────────────────
-        -- Automatically open when a debug session is created
-        -- ────────────────────────────────────────────────────────────────────────────────────────
-        -- local dapui = require("dapui")
-        -- dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
-        -- dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
-        -- dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+            -- C/C++ - codelldb
+            -- NOTE:your code has to be compiled first, e.g., using
+            -- g++ -g main.cpp -o [output_name]
+            -- Then provide [output_name] as the program name
+            dap.adapters.lldb = {
+                type = "executable",
+                command = vim.fn.stdpath("data") .. "/mason/packages/codelldb/codelldb",
+                name = "lldb",
+            }
+            dap.configurations.cpp = {
+                {
+                    type = "lldb",
+                    request = "launch",
+                    name = "[Default] Launch DAP (codelldb) with file selection",
+                    program = function()
+                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                    end,
+                    cwd = "${workspaceFolder}",
+                    stopOnEntry = false,
+                    args = {},
+                },
+            }
+            dap.configurations.c = dap.configurations.cpp
+            dap.configurations.rust = dap.configurations.cpp
 
-        -- Map q to quit in `nvim-dap-view` filetypes
-        vim.api.nvim_create_autocmd({ "FileType" }, {
-            pattern = { "dap-view", "dap-view-term", "dap-repl" }, -- dap-repl is set by `nvim-dap`
-            callback = function(evt)
-                vim.keymap.set("n", "q", "<C-w>q", { silent = true, buffer = evt.buf })
-            end,
-        })
-    end
+            -- Bash - bashdb
+            dap.adapters.bashdb = {
+                type = "executable",
+                command = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
+                name = "bashdb",
+            }
+            dap.configurations.sh = {
+                {
+                    type = "bashdb",
+                    request = "launch",
+                    name = "Launch file",
+                    showDebugOutput = true,
+                    pathBashdb = vim.fn.stdpath("data") ..
+                    "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
+                    pathBashdbLib = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
+                    trace = true,
+                    file = "${file}",
+                    program = "${file}",
+                    cwd = "${workspaceFolder}",
+                    pathCat = "cat",
+                    pathBash = "bash",
+                    pathMkfifo = "mkfifo",
+                    pathPkill = "pkill",
+                    args = {},
+                    env = {},
+                    terminalKind = "integrated",
+                }
+            }
+
+            -- Automatically open when a debug session is created ─────────────────────────────────
+            -- local dapui = require("dapui")
+            -- dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+            -- dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+            -- dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+
+            -- Map q to quit in `nvim-dap-view` filetypes
+            vim.api.nvim_create_autocmd({ "FileType" }, {
+                pattern = { "dap-view", "dap-view-term", "dap-repl" }, -- dap-repl is set by `nvim-dap`
+                callback = function(evt)
+                    vim.keymap.set("n", "q", "<C-w>q", { silent = true, buffer = evt.buf })
+                end,
+            })
+        end
+    },
 }
