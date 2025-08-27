267acb6dfba23828fab493a8e469d2020c32b2d5
Khoi-Nguyen Mac <knmac@amazon.com>
2025-08-27 15:06:30 -0700

    Switch to mainline branch for venv-select

--- lua/plugins/coding/venv_select.lua
+++ lua/plugins/coding/venv_select.lua
@@ -6,7 +6,6 @@ return {
         "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
         -- { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
-    branch = "regexp", -- This is the regexp branch, use this for the new version
     event = "VeryLazy",
     keys = {
         { "<space>e", "<cmd>VenvSelect<cr>" },
