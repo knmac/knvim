2376dc5b258a976a58dcdfbf92c51c728d2e451a
Khoi-Nguyen Mac <knmac@amazon.com>
2025-10-02 09:59:10 -0700

    Remove unnecessary dependency for venv selector

--- lua/plugins/coding/venv_select.lua
+++ lua/plugins/coding/venv_select.lua
@@ -3,7 +3,8 @@ return {
     "linux-cultist/venv-selector.nvim",
     dependencies = {
         "neovim/nvim-lspconfig",
-        "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+        "mfussenegger/nvim-dap",
+        -- "mfussenegger/nvim-dap-python", --optional
         -- { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
     event = "VeryLazy",
