c16ec84ead590f90b8764b94d39886af8a28a3ad
Khoi-Nguyen Mac <knmac@amazon.com>
2025-10-13 20:33:19 -0700

    ui: configure winsep indicator position for 2-window layout

--- lua/plugins/ui/winsep.lua
+++ lua/plugins/ui/winsep.lua
@@ -1,7 +1,11 @@
 -- Highlight the current window
 return {
     "nvim-zh/colorful-winsep.nvim",
-    config = true,
+    opts = {
+        indicator_for_2wins = {
+            position = "center",
+        }
+    },
     -- event = { "BufReadPre", "BufNewFile" },
     event = { "WinLeave" },
 }
