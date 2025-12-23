189c8891d3b7d766d4f2118c24dede3a256a6fef
Khoi-Nguyen Mac <knmac@amazon.com>
2025-12-23 13:12:52 -0800

    feat(oil): add winbar with current directory and enhanced columns
    - Add get_oil_winbar() function to display current directory in winbar
    - Enable additional columns: icon, permissions, size, mtime
    - Configure winbar to show current directory path or buffer name for remote files

--- lua/plugins/utils/oil.lua
+++ lua/plugins/utils/oil.lua
@@ -1,6 +1,18 @@
 -- A vim-vinegar like file explorer that lets you edit your filesystem like a normal Neovim buffer.
 local detail = false
 
+-- Declare a global function to retrieve the current directory
+function _G.get_oil_winbar()
+    local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+    local dir = require("oil").get_current_dir(bufnr)
+    if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+    else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+    end
+end
+
 return {
     "stevearc/oil.nvim",
     event = { "BufReadPre", "BufNewFile" },
@@ -26,12 +38,16 @@ return {
         { "_", function() require("oil").open() end, desc = "Oil: open in the current buffer" },
     },
     opts = {
+        columns = { "icon", "permissions", "size", "mtime", },
         float = { padding = 2, border = "rounded", },
         view_options = { show_hidden = true, },
         confirmation = { border = "rounded", },
         progress = { border = "rounded", },
         ssh = { border = "rounded", },
         keymaps_help = { border = "rounded", },
+        win_options = {
+            winbar = "%!v:lua.get_oil_winbar()",
+        },
         keymaps = {
             ["q"] = { "actions.close", mode = "n" },
             ["gd"] = {
